5ef2f42a44c4492ad8efb5cc0ff0c32af0c3c6a8
EthanWu <wlj0427@gmail.com>
2015-01-22 15:19:24 +0800

    merge conflict.

---
+++ models/msg_friend_request.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+    var MsgFriendRequest = sequelize.define("MsgFriendRequest", {
+        verifyContent: DataTypes.STRING,
+    }, {
+        classMethods: {
+            associate: function (models) {
+                MsgFriendRequest.belongsTo(models.User, {as: 'fromUser', foreignKey: 'from_user_id'});
+                MsgFriendRequest.belongsTo(models.User, {as: 'toUser', foreignKey: 'to_user_id'});
+            }
+        }
+    });
+
+    return MsgFriendRequest;
+};
